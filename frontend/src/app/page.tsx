16dd6457711b4c34ccb5c5a96207b5e8c8190d9f
satoumot <satoumot@systena.co.jp>
2025-07-09 10:21:29 +0900

    jest

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
                     className="dark:invert"
                     src="/next.svg"
                     alt="Next.js logo"
-                    width={170}
+                    width={180}
                     height={38}
                     priority
                 />
